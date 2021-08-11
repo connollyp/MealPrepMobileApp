94b03608d541408ddb86c93e1cf393943a521cb6
hennesseycatwit <hennesseyc@wit.edu>
2021-08-11 09:25:22 -0400

    added user create page

--- MealPrepMobile/app/src/main/java/edu/wit/mobileapp/mealprepmobile/create_account.java
+++ MealPrepMobile/app/src/main/java/edu/wit/mobileapp/mealprepmobile/create_account.java
@@ -3,7 +3,6 @@ package edu.wit.mobileapp.mealprepmobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,62 +13,46 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.Locale;
-
-public class MainActivity extends AppCompatActivity {
+public class create_account extends AppCompatActivity {
+    EditText editTextUsername;
+    EditText editTextPassword;
+    EditText editTextEmail;
+    String email="";
     String username ="";
     String password = "";
 
-    EditText editTextUsername;
-    EditText editTextPassword;
-    Button login;
-    Button createAcc;
-    RequestQueue mQueue;
+
+    Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.account_creation_page);
 
         editTextUsername = (EditText)findViewById(R.id.editTextTextPersonName);
         editTextPassword = (EditText)findViewById(R.id.editTextTextPassword);
-        login = (Button) findViewById(R.id.button);
-        createAcc = (Button) findViewById(R.id.button2);
+        editTextEmail = (EditText)findViewById(R.id.editTextTextEmailAddress);
+        submit = (Button) findViewById(R.id.button);
 
 
 
-        createAcc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, create_account.class);
-                startActivity(intent);
-            }
-        });
-        login.setOnClickListener(new View.OnClickListener(){
+        submit.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 username = editTextUsername.getText().toString().trim();
                 password = editTextPassword.getText().toString().trim();
+                email = editTextEmail.getText().toString().trim();
+
+
 
                 // Instantiate the RequestQueue.
-                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-                String url ="http://3.233.98.252:8080/login?username='" + username + "'&password='" + password + "'";
-
+                RequestQueue queue = Volley.newRequestQueue(create_account.this);
+                String url ="http://3.233.98.252:8080/uploadNewUser?username='" + username + "'&password='" + password + "'&email='" + email + "'";
 
 
                 // Request a string response from the provided URL.
@@ -78,22 +61,16 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onResponse(JSONObject response) {
                                 int userId = 0;
-                                try {
-                                        Intent intent = new Intent(MainActivity.this, main_page.class);
-                                        userId=response.getInt("userId");
-                                        intent.putExtra("userId", userId);
-                                        startActivity(intent);
+                                Intent intent = new Intent(create_account.this, MainActivity.class);
+                                startActivity(intent);
 
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
                                 //Toast.makeText(MainActivity.this, "found user " + userId, Toast.LENGTH_SHORT).show();
 
                             }
                         }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(MainActivity.this, "Please enter a valid username and password", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(create_account.this, "Please enter a valid username and password", Toast.LENGTH_SHORT).show();
 
                     }
                 });
@@ -105,11 +82,5 @@ public class MainActivity extends AppCompatActivity {
 
 
         });
-
     }
-
-
 }
-
-
-
